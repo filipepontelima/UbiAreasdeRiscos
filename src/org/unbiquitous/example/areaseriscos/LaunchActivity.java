4ffe78be50109ec74424752bcb4ca1365142210f
Filipe Lima <filipepontelima@gmail.com>
2014-07-09 15:46:52 -0300

    Grande atualização

--- src/org/unbiquitous/example/areaseriscos/LaunchActivity.java
+++ src/org/unbiquitous/example/areaseriscos/LaunchActivity.java
@@ -1,29 +1,41 @@
 package org.unbiquitous.example.areaseriscos;
 
-import java.util.Arrays;
+import org.unbiquitous.network.http.connection.ClientMode;
+import org.unbiquitous.uos.core.ClassLoaderUtils;
+import org.unbiquitous.uos.core.UOS;
 
-import org.unbiquitous.uImpala.dalvik.GameActivity;
-import org.unbiquitous.uImpala.engine.core.GameSettings;
-import org.unbiquitous.uImpala.engine.io.MouseManager;
-import org.unbiquitous.uImpala.engine.io.ScreenManager;
-
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
-public class LaunchActivity extends GameActivity {
+
+
+public class LaunchActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		run(new GameSettings() {
-			{
-				put("main_activity",LaunchActivity.this);
-				put("first_scene", Menu.class);
-				put("input_managers", Arrays.asList(MouseManager.class));
-				put("output_managers", Arrays.asList(ScreenManager.class));
-
+		
+		ClassLoaderUtils.builder = new ClassLoaderUtils.DefaultClassLoaderBuilder() {
+            public ClassLoader getParentClassLoader() {
+                return LaunchActivity.this.getClassLoader();
+            };
+		};
+		
+		
+		new AsyncTask<Void, Void, Void>() {
+			protected Void doInBackground(Void... params) {
+				
+				UOS midware = new UOS();
+				
+				ClientMode.Properties props = new ClientMode.Properties();
+			    props.setServer("www.my.server.net");
+			    props.put("Activity", this);
+				midware.start(props);
+				return null;
 			}
-		});
+		}.execute();
+		
 	}
 
 }
